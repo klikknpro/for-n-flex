7558a3948e699102212c23d36e9a54e932667044
klikknpro <klikknpro@gmail.com>
2021-11-17 10:33:38 +0100

    End of mentoring session

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-function init() {}
+const generateArray = function (amount) {
+  let returnArray = [];
+  amount = parseInt(amount);
 
-window.addEventListener("load", init);
+  if (!Number.isNaN(amount)) {
+    for (let i = 0; i < amount; i++) {
+      returnArray.push(`${i}`); // szovegge alakitok egy szamot
+    }
+  } else {
+    returnArray.push("error");
+  }
+  return returnArray;
+};
+
+const loadEvent = function () {
+  console.log("The site is ready.");
+  /*
+  console.log(generateArray(10));
+  console.log(generateArray(20));
+  console.log(generateArray("nemnem"));
+  console.log(generateArray([5]));
+  */
+  const root = document.getElementById("root");
+  const list = generateArray(4);
+  if (list[0] !== "error") {
+    for (const item of list) {
+      root.insertAdjacentHTML("beforeend", `<div>${item}</div>`);
+    }
+  }
+};
+
+window.addEventListener("load", loadEvent);
+
+/*
+window.addEventListener("load", function(){
+  full jo ez a verzio is;
+})
+window.addEventListener("load", () => {
+  meg ez is f@sza;
+})
+*/
